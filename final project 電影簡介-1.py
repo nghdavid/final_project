38ef8f59b744305b88b2c5d577e691d5207d6069
yuhsiangw <51158664+yuhsiangw@users.noreply.github.com>
2019-05-30 21:26:52 +0800

    final project 電影簡介-1
    要跑程式之前記得下載chrome驅動程式，並修改本程式中的路徑到該驅動程式的位置

---
+++ final project 電影簡介-1.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+
+data = input()
+typelist = []
+#chrome_options = Options()  
+#chrome_options.add_argument("--headless")
+chromedriver = 'C:\\Users\\user\\Desktop\\chromedriver'
+driver = webdriver.Chrome(chromedriver)
+driver.get('http:google.com')
+q = driver.find_element_by_name('q')
+q.send_keys(data + ' Yahoo電影')
+q.send_keys(Keys.RETURN)
+a = driver.find_element_by_css_selector('#rso a:nth-child(1)')
+a.click()
+soup = BeautifulSoup(driver.page_source)
+print('類型: ', end = '')
+for i in soup.findAll('div', {'class':'level_name_box'}):
+    for j in i.findAll('div', {'class':'level_name'}):
+        typem = j.get_text()
+        typem = typem.strip()
+        print(typem, end = ' ')
+
+print()
+print()
+
+for i in soup.find('div', {'class':'movie_intro_info_r'}).findAll('span')[0]:
+            typem = i
+            typem = typem.strip()
+            print(typem)
+            break
+for i in soup.find('div', {'class':'movie_intro_info_r'}).findAll('span')[1]:
+            typem = i
+            typem = typem.strip()
+            print(typem)
+            break
+for i in soup.find('div', {'class':'movie_intro_info_r'}).findAll('span')[3]:
+            typem = i
+            typem = typem.strip()
+            print(typem)
+            break
+driver.back()
+driver.back()            
+print()
+
+print('簡介: ', end = '')
+q = driver.find_element_by_name('q')
+q.send_keys(data + ' 開眼電影')
+q.send_keys(Keys.RETURN)
+a = driver.find_element_by_css_selector('#rso a:nth-child(1)')
+a.click()
+soup = BeautifulSoup(driver.page_source)
+for i in soup.find('div', {'id':'filmTagBlock'}).findAll('span')[2]:
+    brief = i
+    brief = str(brief)
+    brief = brief.strip()
+    print(brief)
+    break
+    
+print()
+
+
+driver.close()
